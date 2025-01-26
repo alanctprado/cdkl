33b3a487fd241adc7d1ab73c5dbe30f7cca53b42
Alan Prado <alanctprado@gmail.com>
2025-01-26 19:16:02 -0300

    minor change to dpll

--- src/main/kotlin/DPLL.kt
+++ src/main/kotlin/DPLL.kt
@@ -14,17 +14,17 @@ class DPLL(private val clauses: Formula) {
     return model.map { Literal(it.key, if (it.value == True) Polarity.Positive else Polarity.Negative) }
   }
 
-  private fun propagate(formula: Formula, literal: Literal): Formula {
+  private fun propagate(clauses: Formula, literal: Literal): Formula {
     model[literal.variable] = if (literal.isPositive) True else False
-    return formula.removeSatisfiedClauses(literal).removeOppositeLiterals(literal)
+    return clauses.removeSatisfiedClauses(literal).removeOppositeLiterals(literal)
   }
 
-  private fun recursiveDpll(formula: Formula): SolverResult {
-    if (formula.isEmpty) return Satisfiable
-    if (formula.hasEmptyClause) return Unsatisfiable
-    formula.unitClause?.getLiteral?.let { return recursiveDpll(propagate(formula, it)) }
-    val literal: Literal = formula.getLiteral!!
-    if (recursiveDpll(propagate(formula, literal)) == Satisfiable) return Satisfiable
-    return recursiveDpll(propagate(formula, literal.opposite()))
+  private fun recursiveDpll(clauses: Formula): SolverResult {
+    if (clauses.isEmpty) return Satisfiable
+    if (clauses.hasEmptyClause) return Unsatisfiable
+    clauses.unitClause?.getLiteral?.let { return recursiveDpll(propagate(clauses, it)) }
+    val literal: Literal = clauses.getLiteral!!
+    if (recursiveDpll(propagate(clauses, literal)) == Satisfiable) return Satisfiable
+    return recursiveDpll(propagate(clauses, literal.opposite()))
   }
 }
